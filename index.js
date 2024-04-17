86778571072311f90d6955ce1378a0734988a228
Philip Karanja <kpk.my.mailings@gmail.com>
2024-04-17 18:28:09 -0500

    filter function

--- index.js
+++ index.js
@@ -4,3 +4,46 @@ console.log(color.red("Boring message for practice"));
 
 console.log(color.blue("Blue Men group"))
 
+const numbers = [1, 2, 3, 4, 5];
+
+function addOne(array) {
+    for (let i=0; i <array.length; i++){
+        console.log(color.red(array[i] + 1));
+    }
+}
+
+addOne(numbers);
+
+numbers.forEach(function(num) {
+    console.log(color.green(num + 1));
+});
+
+numbers.forEach((num) =>{
+    console.log(color.red(num + 1))
+}
+
+);
+
+//holy trinity of programming: Map Reduce Filter
+
+//FILTER
+
+//filtering numbers example
+
+// function isOdd(array, oddArr = []) {
+//     for (let i = 0; i < array.length; i++){
+//         if (array[i] % 2 ! == 0) {
+//             oddArr.push(array[i]);
+//         }
+//     }
+//     return oddArr
+// }
+
+// const oddArray = isOdd(numbers);
+// console.log(oddArray);
+
+const easyOddArray = numbers.filter((num) =>  num % 2 !== 0);
+
+
+
+console.log(easyOddArray);
